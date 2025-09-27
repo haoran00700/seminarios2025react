2490e797360a9e91d6e3984d35ada4b3023c7736
haoran00700 <haoran.huang@sga.pucminas.br>
2025-09-26 21:45:22 -0300

    imagem-import

--- hello-world/src/App.jsx
+++ hello-world/src/App.jsx
@@ -2,13 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import banana from './assets/banana.jfif'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   <div>
-    <h1>hello world {2+2}</h1> 
+    <h1>Oficina react</h1>
+    <h2>{Date()}</h2>
+    <h3>{Number()}</h3> 
+       <a id = "link" style={{color :"red"}} href = "https://www.google.com/">google</a>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JAVASCRIPT</li>
+      <li>Introducao React </li>
+      <li>Hello World</li>
+    </ul>
+    <p>lista estatica utilizando do react </p>
+    <img src='/macaco.jfif' width="100" height="100"></img>
+    <br/>
+    <img src={banana} width="100" height="100"></img>
   </div>
   )
 }
---
+++ hello-world/src/components/PrimeiroCom.jsx
@@ -0,0 +1,9 @@
+import '../App.css'
+
+function PrimeiroComp(){
+    return (
+        <div>
+            <h2>meu primeiro component {2+2}</h2>
+        </div>
+    )
+}
