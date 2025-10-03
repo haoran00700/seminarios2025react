f85c20fee370d68631293a98f832a13f7c5e5b48
haoran00700 <haoran.huang@sga.pucminas.br>
2025-10-03 19:30:30 -0300

    mensagem.jsx

---
+++ hello-world/src/components/mensagem.jsx
@@ -0,0 +1,11 @@
+import '../App.css'
+
+function Mensagem({ titulo , subtitulo}) {
+    return (
+        <div>
+            <h1>{tiitulo}</h1>
+            <p>{subtitulo}</p>
+        </div>
+    )
+}
+export default Mensagem 
