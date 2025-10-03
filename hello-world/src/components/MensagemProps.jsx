1afb3333f9ca9fec1d5e4b92d7f26868dd868dee
haoran00700 <haoran.huang@sga.pucminas.br>
2025-10-03 20:06:57 -0300

    mensagem-props

---
+++ hello-world/src/components/MensagemProps.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function MensagemProps(props) {
+    return (
+        <div>
+            <h1>{props.titulo}</h1>
+            <p>{props.subtitulo}</p>
+        </div>
+    )
+}
+export default MensagemProps
--- hello-world/src/main.jsx
+++ hello-world/src/main.jsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import PrimeiroComp from './components/PrimeiroCom.jsx'
 import Mensagem from './components/mensagem.jsx'
+import MensagemProps from './components/MensagemProps.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <PrimeiroComp/>
    <Mensagem titulo = "ola haoran!" subtitulo = "Esse e um exemplo de props no React" />
+    <MensagemProps titulo = "oficina" subtitulo = "react"/>
+    <MensagemProps titulo = "oficina react " subtitulo = "turma noite"/>
     <App />
   </StrictMode>
 )
